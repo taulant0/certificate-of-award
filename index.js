706abe2fcdba627b9740d473d78d265d4ad97b87
Taulant Shala <159620763+taulant0@users.noreply.github.com>
2024-10-16 17:30:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("solution.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const fullName = req.body["fName"] + " " + req.body["lName"];
+  const reason = req.body["reason"];
+  const currentDay = new Date().getDate();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentYear = new Date().getFullYear();
+  const currentDate = `${currentDay}.${currentMonth}.${currentYear}`;
+
+
+
+
+  res.render("solution.ejs", { fullName, reason, currentDate });
+
+
+
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
